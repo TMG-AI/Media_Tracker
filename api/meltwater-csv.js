0cac32bcbd81737b6430f1cea87e4a7b2b098316
TMG-AI <swheatman@signalinteractive.com>
2025-08-14 23:51:20 -0400

    Update meltwater-csv.js

--- api/meltwater-csv.js
+++ api/meltwater-csv.js
@@ -1,6 +1,6 @@
 // Meltwater CSV processing endpoint
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // Enable CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
